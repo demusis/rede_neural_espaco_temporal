4aeb89910a1538546c2868badd20eb5f8ebfe50d
Kaue Arima <kauearima@gmail.com>
2020-07-06 16:53:24 -0400

    Ajustando o Git com os arquivos do Drive

--- ClassMatrix.py
+++ ClassMatrix.py
@@ -1,32 +1,61 @@
-from ClassNo import No
+from ClassRedeNeural import RedeNeural
+import pyKriging
+from pyKriging.krige import kriging
+from pyKriging.samplingplan import samplingplan
+from pyKriging.testfunctions import testfunctions
+import pandas as pd
 
 
 class Matrix:
 
-    def __init__(self, tamanho):
-        self.length = 0
-        self.matrix = []
-        self.row = []
-        self.tamanho = tamanho
+    def __init__(self, colunas, redeneural):
+        self.matrix = pd.DataFrame(columns = colunas)
+        self.colunas = colunas
+        self.redeneural = redneural
+        self.kikage = None
+        self.X = None
+        self.y = None
+        
 
-    def setNo(self, no: No):
-        if self.length % self.tamanho == 0:
-            self.row.append(no)
-            self.matrix.append(self.row)
-            self.row = []
-            self.length += 1
+    def setLinhaMatrix(self, item):
+        self.matrix.append(item)
+        
+    def setNovaColunaMatrix(self, item):
+        self.matrix[item] = []
+
+    def getShapeMatrix(self):
+        return self.matrix.shape
+
+    def getItemMatrix(self, i:int):
+        return self.matrix[i]
+    
+    def getTotalElementosLinhaMatrix(self, i:int):
+        return self.matrix[i].count
+    
+    def getColunasMatrix(self):
+        return self.matrix.columns
+    
+    def getMatrix(self):
+        return self.matrix
+    
+    def krigagemMatrix(self, colY,colX,colZ):
+        
+        self.X = list(self.matrix[colY, colX,colZ])
+        testfun = testfunctions().squared
+        self.y = testfun(self.X)
+        self.k = kriging(self.X, self.y, testfunction=testfun, testPoints=300)
+        
+        self.k.train()
+        self.k.snapshot()
+        
+        
+    def predicaoElementoKrigagem(self,elemento, i:int):
+        pred_gen = self.k.predict(elemento)
+        if 'krigagem' in self.matrix.columns:
+            self.matrix[i]['krigagem'] = pred_gen
         else:
-            self.row.append(no)
-
-    def getNo(self, i:int, j:int):
-        if i == self.length:
-            if j <= self.tamanho:
-                return self.row[j]
-        else:
-            if i < self.length:
-                if j <= self.tamanho:
-                    return self.matrix[i][j]
-        return False
-
-    def finalizaMatrix(self):
-        self.matrix.append(self.row)
+            self.setNovaColunaMatrix('krigagem')
+            
+        return predictions_gen
+        
+        
